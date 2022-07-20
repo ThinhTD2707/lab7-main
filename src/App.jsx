eb7b6bce5a3c195f48470ea5fd30177c3e7c136c
thinh <thinhtdse150375@fpt.edu.vn>
2022-07-20 20:06:00 +0700

    lab8

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,18 @@ import './App.css';
 const store = ConfigureStore();
 
 class App extends Component {
-  
+
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          {/* <div className="App"> */}
           <div>
             <Main />
           </div>
         </BrowserRouter>
       </Provider>
-      // <BrowserRouter>
-      //   <div className="App">
-      //     <Main />
-      //   </div>
-      // </BrowserRouter>
     );
-  }  
+  }
 
 }
 
